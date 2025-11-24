ce70615ce61087a6eac9c0b1975139bca434510b
Onur Odyakmaz <41840769+OnurOd@users.noreply.github.com>
2025-11-24 14:13:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import sys
-from PySide6.QtWidgets import QApplication
+import os
+
+# Add the project root to Python path so "src" is importable
+sys.path.append(os.path.dirname(__file__))
+
 from src.gui.main_window import MainWindow
+from PySide6.QtWidgets import QApplication
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
